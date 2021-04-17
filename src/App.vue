6f31f8d4e958b42873fa0911197edc7bc2d08fd7
website-development <website.dewelopment@gmail.com>
2021-04-17 09:39:28 +0300

    17.04.2021

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <v-btn v-if="$route.path !== '/'" to="/" icon>
+        <v-icon>mdi-arrow-left</v-icon>
+      </v-btn>
+      <v-toolbar-title>Veather</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <router-view />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App"
+});
+</script>
+
+<style lang="scss">
+@for $i from 1 through 10 {
+  .gap-#{$i} {
+    gap: 0.5rem * $i;
+  }
+}
+</style>
---
+++ src/router/index.ts
@@ -0,0 +1,24 @@
+import Vue from "vue";
+import VueRouter, { RouteConfig } from "vue-router";
+import Home from "../views/Home.vue";
+
+Vue.use(VueRouter);
+
+const routes: Array<RouteConfig> = [
+  {
+    path: "/",
+    name: "Home",
+    component: Home
+  },
+  {
+    path: "/full/:id",
+    name: "FullReport",
+    component: () => import("../views/FullReport.vue")
+  }
+];
+
+const router = new VueRouter({
+  routes
+});
+
+export default router;
